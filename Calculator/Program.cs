7108eeda533434cf35417528bf41b50c84624a31
Maxim Dobrynin <dobrynin-m@ulster.ac.uk>
2025-09-28 22:08:07 +0100

    Implemented program code.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,8 +1,11 @@
 ﻿Console.WriteLine("Type in the first number followed by the Enter key");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Type the second number, and then press Enter");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
+
 // Perform calculation
 int result = firstNumber + secondNumber;
+
 // Output answer to console
 Console.WriteLine("Adding {0} and {1} gives the answer {2}", firstNumber, secondNumber, result);
